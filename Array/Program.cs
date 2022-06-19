5938585365c9d6df63e2dd681494a6793faa1fd9
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-20 01:12:54 +0200

    LA POO

--- Array/Program.cs
+++ Array/Program.cs
@@ -4,20 +4,29 @@ namespace ArrayDemo
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
+
         {
 
             string[] tableau1 = { "Zoé", "Marc", "Daouda","Jean",  "Alexandre" }; 
             string [] tableau2 = new string[tableau1.Length];
             //on peut utiliser array.copy aussi 
-            //Array.Copy(tableau2 , tableau1, tableau1.Length);
-            Array.Clear(tableau2, 0, 2); 
-            Console.WriteLine(tableau2.Length);
+         //rray.Copy(tableau2 , tableau1, tableau1.Length);
+           //rray.Resize(ref tableau2, 10);
+            Console.WriteLine("Index de Daouda " + Array.IndexOf(tableau1, "Daouda")); 
+            //Array.Clear(tableau2, 0, 2); 
+            //array.short(tableau2); 
+
+            //Console.WriteLine(tableau2.Length);
+
             //tableau1 = 2
-            foreach(string tableau in tableau1)
+           /* foreach(string tableau in tableau1)
             {
                 Console.WriteLine(tableau);
-            }
+            }*/
+
+            //array.reverse 
+
             Console.ReadLine();
         }
 
