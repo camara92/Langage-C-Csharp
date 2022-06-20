73e50de92ab8754c7f364dde265b4f395696f379
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-20 02:03:16 +0200

    CLASS, ATRIBUTS ET INSTANCIATIONS

---
+++ POO/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    internal class Animal
+    {   //si on ne spécfie rien il est en private 
+       public string Nom;
+      public int Age; 
+      //méthode 
+      public void AfficheInfo()
+        {
+            Console.WriteLine("Je suis " + Nom);
+            Console.WriteLine("J'ai " + Age + "an(s)");
+
+        }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace POO
+{
+    class PROGRAM
+    {
+        static void Main(string[] args)
+        {
+            Animal monAnimal = new Animal();
+            monAnimal.Age = 2;
+            monAnimal.Nom = "Chat"; 
+
+            Console.WriteLine(monAnimal.Nom);
+            Console.WriteLine(monAnimal.Age);
+
+            Animal monAnimal2 = new Animal();
+            monAnimal2.Age = 20;
+            monAnimal.Nom = "Chien";
+
+            monAnimal.AfficheInfo(); 
+
+            Console.ReadLine();
+
+        }
+    }
+}
