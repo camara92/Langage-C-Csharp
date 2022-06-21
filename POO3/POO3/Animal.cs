0cbb2171826e8135d6e793dc439e475556e795c0
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-21 12:27:10 +0200

    Methoes et constructeurs

---
+++ POO3/POO3/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO3
+{
+    internal class Animal
+    {
+        //les attributs 
+        public string Nom;
+        public int Age;
+
+        //constructeur par défaut 
+        public Animal()
+        {
+           // Console.WriteLine("J'ai ");
+           //intérêt de faire passer les infos des attributs 
+           //en le supprimant on ne peut passer d'autres infos sans qu'il y ait des infos sur ce dernier 
+           //ne s'exécutant qu'au moment de l'instanciation 
+
+        }
+
+        public Animal(string Nom, int Age)
+        {
+            this.Nom = Nom;
+            this.Age = Age;
+        }
+       
+
+
+        //méthodes 
+        public void AfficheInfo()
+        {
+            Console.WriteLine("Je suis "+Nom);
+            Console.WriteLine("J'ai " + Age); 
+        }
+    }
+}
---
+++ POO3/POO3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace POO3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal monAnimal = new Animal("britus", 2);
+            /*monAnimal.Nom = "Bengo";
+            monAnimal.Age = 3;
+            Console.WriteLine("Nom de l'animale est : " + monAnimal.Nom);
+            Console.WriteLine("L'age de l"+ monAnimal.Nom + " est : " + monAnimal.Age);*/
+            monAnimal.AfficheInfo(); 
+            Console.ReadLine();
+
+            //utilisation de la méthode 
+        }
+    }
+}
