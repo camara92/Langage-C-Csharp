f5759eed6abe20531fe0a1e7f9e70d92d044128f
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-22 05:49:19 +0200

    Creation base de données en SQLITE + installation package

---
+++ BDDSQLITE/BDDSQLITE/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Data.SQLite;
+ 
+namespace Base_de_donnees_sqlite
+{
+    class PROGRAM
+    {
+        static void Main(string[] args)
+        {
+            //créer la bdd 
+            string bddpath = "d:\\data\\bdd.sqlite";
+            if (!File.Exists(bddpath)) CreateBdd(); 
+            void CreateBdd()
+            {
+                SQLiteConnection.CreateFile(bddpath);   
+                SQLiteConnection con = new SQLiteConnection("Data Source=d:\\data\\bdd.sqlite;Version=3;");
+                con.Open();
+
+                //créer une requête 
+                string sql = "create table clients (nom varchar(25), prenom varchar(20)) ";
+                SQLiteCommand command = new SQLiteCommand(sql, con);    
+                command.ExecuteNonQuery();
+                con.Close(); 
+            }
+            
+        }
+    }
+}
