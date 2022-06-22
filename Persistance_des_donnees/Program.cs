10649fec5b7a1d5bf5c98f1e1868d892b2aeee92
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-22 03:42:13 +0200

    Persistance des données en C-Sharp #C

---
+++ Persistance_des_donnees/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//importer IO
+using System.IO;
+
+namespace Persistance_des_donnees
+{
+    class PROGRAM
+    {
+        static void Main(string[] args)
+        {
+            /*using(StreamWriter sw =new StreamWriter("d:\\data\\test.txt"))
+            {
+                sw.WriteLine("1ère ligne");
+            }*/
+           /* using (StreamWriter sw = File.AppendText("d:\\data\\test.txt"))
+            {
+                //Chaque exécution sera ajouté au fichier 
+                sw.WriteLine("Bonjour est ajouté ");
+            }*/
+
+           /*using (StreamReader sr= new StreamReader("d:\\data\\test.txt"))
+            {
+                string line = null;
+                line = sr.ReadLine(); 
+                while(line!= null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+            }
+          */
+           using (StreamReader sr= new StreamReader("d:\\data\\test.txt"))
+            {   //lire tout 
+                string file = sr.ReadToEnd();
+                Console.WriteLine(file);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
