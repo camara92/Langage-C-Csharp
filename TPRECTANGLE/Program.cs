fb515085e766da325f02cfa17d9fc9ac43c5efe7
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-22 02:43:33 +0200

    EXERCICE TP POO

---
+++ TPRECTANGLE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TPRECTANGLE
+{
+    class PROGRAM
+    {
+        static void Main(string[] args)
+        {
+            //si float on met un f 
+            Rectangle rec = new Rectangle(17.5f, 25.3f, "Rouge");
+            Rectangle rec1 = new Rectangle(158.5f, 215.3f);
+            rec.Perimetre();
+            rec1.Perimetre();
+            //rec.Couleur = noir;
+            Console.WriteLine(rec.Couleur);
+            Console.WriteLine(rec1.Couleur);
+            Console.WriteLine("L'aire est "+rec.Aire());
+            rec.AfficheCouleur();
+            rec1.AfficheCouleur();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TPRECTANGLE/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPRECTANGLE
+{
+    internal class Rectangle
+    {
+        private float _longueur;
+        private float _largeur;
+
+        private string _couleur; 
+
+        public string Couleur
+        { 
+            //uniquement en lecture 
+            get { return _couleur; } 
+        }
+        //constructeur 
+        public Rectangle(float longueur, float largeur)
+        {
+            this._longueur = longueur;
+            this._largeur = largeur;
+        }
+        //deuxieme constructeur pour gérer la couleur 
+        public Rectangle(float longueur, float largeur, string couleur)
+        {
+            this._longueur = longueur;
+            this._largeur = largeur;
+            this._couleur = couleur;
+        }
+        //methode qui permet d'afficher le périmètre dans la console 
+        public void Perimetre()
+        {
+            Console.WriteLine($"Le périmètre est de { (_longueur * _largeur )*2 } mètre(s)");
+        }
+        //aire retrournée 
+        public float Aire()
+        {   
+            return _longueur* _largeur;
+        }
+        public void AfficheCouleur()
+        {
+            string str = _couleur != null ? "Ma couleur est " + _couleur : "Aucune couleur."; 
+            Console.WriteLine(str);
+        }
+
+    }
+}
