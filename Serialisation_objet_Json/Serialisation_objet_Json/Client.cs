4f14b07b3759146251780d3cbf57539be05e822a
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-22 04:26:19 +0200

    SERIALISATION ET DESERIALISATION JSON

---
+++ Serialisation_objet_Json/Serialisation_objet_Json/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialisation_objet_Json
+{
+    internal class Client
+    {
+        private string _nom;
+        private string _prenom; 
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+        public string Prenom
+        {
+            get { return _prenom; }
+            set { _prenom = value; }
+        }
+        public Client(string nom, string prenom)
+        {
+            this._nom = nom; 
+            this._prenom= prenom;
+        }
+        //constructeur par défuaut 
+
+        public Client() { }
+        
+        public void Information()
+        {
+            Console.WriteLine("Bonjour, je suis " + _nom + " " + _prenom);
+        }
+    }
+}
---
+++ Serialisation_objet_Json/Serialisation_objet_Json/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic; 
+namespace Serialisation_objet_Json
+{
+    class PROGRAM
+    {
+        
+        static void Main(string[] args)
+        {
+            /*Client c1 = new Client("CAMARA", "Daouda");
+            Client c2 = new Client("Lucienne", "Sylla");
+            Client c3 = new Client("Camus", "Diallo");*/
+
+            List<Client> listeCLient = new List<Client>();
+           /* listeCLient.Add(c1);
+            listeCLient.Add(c2);
+            listeCLient.Add(c3);*/
+
+
+            //seralisaer 
+            /* string JsonString = JsonSerializer.Serialize(listeCLient);
+             File.WriteAllText("d:\\data\\listedata.json", JsonString);
+             */
+
+
+
+
+            //Déserializer 
+            string JsonString = File.ReadAllText("d:\\data\\listedata.json"); 
+           listeCLient = JsonSerializer.Deserialize<List<Client>>(JsonString);
+           // c2.Information();
+           foreach(Client client in listeCLient)
+            {
+                client.Information();
+            }
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
