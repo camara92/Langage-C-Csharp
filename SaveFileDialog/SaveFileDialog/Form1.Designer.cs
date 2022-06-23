ef9083d708230203450dc0f2ad58e5938ee707d7
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-23 16:57:14 +0200

    Sauvegarde des fichiers en text et PDF

---
+++ SaveFileDialog/SaveFileDialog/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SaveFileDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(600, 364);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(635, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enregistrer sous ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ SaveFileDialog/SaveFileDialog/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace SaveFileDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //saveFileDialog1.Filter = "Fichier texte (*.txt)|*.txt*";
+            saveFileDialog1.Filter = "Fichier texte (*.txt)|*.txt| Fichier PDF(*.pdf)|*.pdf";
+            saveFileDialog1.Title = "Enregistrer votre fichier svp ";
+            saveFileDialog1.ShowDialog(); 
+            //recuperer 
+            if(saveFileDialog1.FileName !="")
+            {
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        MessageBox.Show("Enregistrement au format txt");
+                        break; 
+                    case 2:
+                        MessageBox.Show("Enregistrement au format PDF");
+                        break; 
+
+                }
+               StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+                sw.Write(textBox1.Text);
+                sw.Close(); 
+            }
+        }
+    }
+}
