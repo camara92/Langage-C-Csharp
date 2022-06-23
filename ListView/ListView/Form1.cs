88a742a9439aa177de74aa9fd814498ab9b76974
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-23 03:27:21 +0200

    ListView en #C

---
+++ ListView/ListView/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string initial = textBox1.Text;
+            string prenom = textBox2.Text;
+            string nom = textBox3.Text;
+
+            //ajout 
+            ListViewItem ligne = new ListViewItem(initial);
+            ligne.SubItems.Add(nom);
+            ligne.SubItems.Add(prenom); 
+
+            listView1.Items.Add(ligne);
+        }
+    }
+}
---
+++ Tootip/Tootip/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tootip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = "Valeur de la trrackbar " + trackBar1.Value; 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //modifier 
+            trackBar1.Value += 10;
+           // trackBar1.Value = 0;
+            label1.Text = "Valeur de la trrackbar " + trackBar1.Value;
+
+        }
+    }
+}
