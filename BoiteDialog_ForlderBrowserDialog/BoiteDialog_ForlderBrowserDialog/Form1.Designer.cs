9c942b4acba86d11dc7070fcd84c34f5b5ff3bbf
camara92 <88945535+camara92@users.noreply.github.com>
2022-06-24 02:25:28 +0200

    Premier projet sous ASP.NET

---
+++ BoiteDialog_ForlderBrowserDialog/BoiteDialog_ForlderBrowserDialog/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BoiteDialog_ForlderBrowserDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(3, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Selection d\'un chemin";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "description";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ BoiteDialog_ForlderBrowserDialog/BoiteDialog_ForlderBrowserDialog/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoiteDialog_ForlderBrowserDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //méthode qui a pour but de ne pas créer un folderbrowserdialog 
+            /*
+            FolderBrowserDialog monFolder = new FolderBrowserDialog();
+            monFolder.ShowNewFolderButton = true; 
+            monFolder.Description = "Ma description";
+            monFolder.RootFolder = Environment.SpecialFolder.Desktop;
+            monFolder.ShowDialog(); 
+             */
+            folderBrowserDialog1.Description = "Une description";
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.ProgramFiles; 
+            //folderBrowserDialog1 = new FolderBrowserDialog();
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //recherche d'un dossier 
+                label1.Text = folderBrowserDialog1.SelectedPath; 
+
+            }
+        }
+    }
+}
